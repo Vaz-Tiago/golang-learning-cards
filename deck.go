eb9cad35a5ae02fa5b8b8e5a704df8d10294a86b
Tiago Vaz <tiago.vaz@hotmail.com>
2022-10-25 18:13:59 -0300

    multiple returning functions

--- deck.go
+++ deck.go
@@ -11,3 +11,22 @@ func (d deck) print(){
 		fmt.Println(i, card)
 	}
 }
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four" } // "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
-func main () {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	cards.print()
-}
+import "fmt"
 
-func newCard() string {
-	return "Five of Diamonds"
+func main () {
+	cards := newDeck()
+	hand, remainCards := deal(cards, 5)
+
+	hand.print()
+	fmt.Println("------------")
+	remainCards.print()
 }
