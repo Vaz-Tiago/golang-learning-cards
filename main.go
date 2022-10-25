a694eb71054da0418159f65a622c9b3f2904b18c
Tiago Vaz <tiago.vaz@hotmail.com>
2022-10-25 18:45:11 -0300

    save a file on hard driver

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
-import "fmt"
-
 func main () {
 	cards := newDeck()
-	hand, remainCards := deal(cards, 5)
+	cards.saveToFile("my_cards")
+	// hand, remainCards := deal(cards, 5)
 
-	hand.print()
-	fmt.Println("------------")
-	// remainCards.print()
-	fmt.Println(remainCards.toString())
+	// hand.print()
+	// fmt.Println("------------")
+	// // remainCards.print()
+	// fmt.Println(remainCards.toString())
 }
